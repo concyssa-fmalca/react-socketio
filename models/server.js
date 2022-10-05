1eea7c7af7a0f92c6fabba33f1d4d4d7c9830161
fmalca <67565141+concyssa-fmalca@users.noreply.github.com>
2022-10-05 17:30:59 -0500

    Se habilitó CORS el server

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketio =  require('socket.io')
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 class Server {
 
@@ -20,6 +21,8 @@ class Server {
     middlewares() {
         //Desplegar directorio público
         this.app.use( express.static( path.resolve(__dirname,'../public')));
+
+        this.app.use(cors());
     }
 
     configurarSockets() {
