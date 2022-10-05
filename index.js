23735464c340aaca3f97f55b9d234f6828af87a7
fmalca <67565141+concyssa-fmalca@users.noreply.github.com>
2022-10-05 12:08:45 -0500

    Inicial en Heroku

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-//Servidor de Express
-const express = require('express');
-const app = express();
+const Server = require("./models/server");
 
-//Servidor de sockets
-const server = require('http').createServer(app);
+require('dotenv').config();
 
-//Configuración del socket server
-const io = require('socket.io')(server);
+const server = new Server();
 
-//Desplegar directorio público
-app.use( express.static(__dirname + '/public'));
-
-//Escuchar peticiones del cliente (socket)
-io.on('connection',( socket ) =>{
-    socket.on('message-to-server', (data) => {
-        console.log(data);
-        io.emit('message-from-server',data);
-    })
-})
-
-server.listen(8080, () =>{
-    console.log("Server corriendo en el puerto : 8080");
-});
-
-
+server.execute();
---
+++ models/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const socketio =  require('socket.io')
+const path = require('path');
+const Sockets = require('./sockets');
+
+class Server {
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Http server
+        this.server = http.createServer(this.app);
+
+        //Configuración de sockets 
+        this.io = socketio(this.server, {/* configuraciones */});
+    }
+
+    middlewares() {
+        //Desplegar directorio público
+        this.app.use( express.static( path.resolve(__dirname,'../public')));
+    }
+
+    configurarSockets() {
+        new Sockets( this.io);
+    }
+
+    execute() {
+
+        // Inicializar Middlewares
+        this.middlewares();
+
+        //Configurar sockets
+        this.configurarSockets();
+
+        // Inicializar Server
+        this.server.listen(this.port, () =>{
+            console.log("Server corriendo en el puerto :", this.port);
+        });
+    }
+}
+
+module.exports = Server;
